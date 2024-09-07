4724f513a379bd9ffebca40dc2041cb89c9a2208
elglus <elglus11@gmail.com>
2024-09-07 10:38:17 +0300

    Add THE RIGHT file

--- scraper.py
+++ scraper.py
@@ -1,4 +1,14 @@
-from urllib import request
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import re
 
-pages = request.urlopen("https://market.yandex.ru/product--macbook-air-13-2024/77121092?sku=102832949157&uniqueId=901698")
-print(pages)
+url = "https://market.yandex.ru/product--macbook-air-13-2024/77121092?sku=102833204153&uniqueId=16327879&do-waremd5=eU2yNcDhY3IAuvI5gCgpbw&sponsored=1&nid=26895412"
+
+driver = webdriver.Chrome()
+driver.get(url)
+driver.implicitly_wait(1)
+
+price = driver.find_element(by=By.CLASS_NAME, value="Jdxhz").text
+price = re.sub(r"\D", "", price)
+
+print(price)
